43d0f962f00c2a3f9bf399f8fd9f406963b872ba
Johanna Forssén <forssen.johanna@gmail.com>
2020-11-07 20:47:18 +0100

    Add projects

---
+++ StepManiaSongRenamer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StepManiaSongRenamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
